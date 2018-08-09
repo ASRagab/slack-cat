61b7e029c6e36bfc0a9ed6472a26539ad791ac2f
Ahmad Ragab <aragab@stashinvest.com>
2018-08-08 23:16:30 -0400

    Adding rudimentary blacklist functionality to pooping, update README.md

--- modules/poop/index.js
+++ modules/poop/index.js
@@ -3,6 +3,7 @@
 const botParams = {
   icon_emoji: ':poop:',
   username: 'PoopCat',
+  blacklist: 'channels_blacklist'
 };
 
 const userPattern = new RegExp(/\<@(.*.)\>/, 'i');
@@ -10,12 +11,22 @@ const userPattern = new RegExp(/\<@(.*.)\>/, 'i');
 module.exports = class Poop extends BaseStorageModule {
   async handle(data) {
 
+    if (!this.isValidConfig()) {
+      this.bot.postMessage(data.channel, `Please add key '${botParams.blacklist}' to 'config.json'`);
+      return;
+    }
+
+    if (this.isBlackListed(data.channel)) {
+      this.denyPooping(data.channel);
+      return;
+    }
+
     if (data.cmd === 'poop-board' || data.cmd === 'poopboard') {
       this.postLeaderBoard(data);
       return;
     }
 
-    if (data.cmd === 'poops') {      
+    if (data.cmd === 'poops') {
       this.postUserPoops(data);
       return;
     }
@@ -23,14 +34,23 @@ module.exports = class Poop extends BaseStorageModule {
     this.updateAndPostPoop(data);
   }
 
+  isValidConfig() {
+    return config.getKey(botParams.blacklist) && config.getKey(botParams.blacklist).poop;
+  }
+
+  async denyPooping(channel) {
+    const msg = `Not in ${channel} please!`;
+    this.bot.postMessage(channel, msg);
+  }
+
   async updateAndPostPoop(data) {
     const poopData = await this.updatePoop(data);
     let msg = `*${poopData.user} just got pooped!!!* ${poopData.user} pooped _${poopData.poops} times_.\nPlease lock your screen next time! http://osxdaily.com/2011/01/17/lock-screen-mac/\n`;
     if (poopData.poops === 1) {
-      msg = `*${poopData.user}* got pooped for the first time. That's a big stink! Don't leave your computer unlocked when you're not with it! Side effects can include hacking, stealing, destruction, and...pooping (by one of your sneaky gremlin coworkers). http://osxdaily.com/2011/01/17/lock-screen-mac/`;      
+      msg = `*${poopData.user}* got pooped for the first time. That's a big stink! Don't leave your computer unlocked when you're not with it! Side effects can include hacking, stealing, destruction, and...pooping (by one of your sneaky gremlin coworkers). http://osxdaily.com/2011/01/17/lock-screen-mac/`;
     }
 
-  
+
     this.bot.postMessageWithParams(data.channel, msg, botParams);
   }
 
@@ -65,27 +85,25 @@ module.exports = class Poop extends BaseStorageModule {
   }
 
   async getUserName(userId) {
-    const user = await this.bot.getUserNameFromId(userId);        
-    return user.user.profile.display_name || user.user.name;    
+    const user = await this.bot.getUserNameFromId(userId);
+    return user.user.profile.display_name || user.user.name;
   }
 
   async postUserPoops(data) {
     let user = data.user_text;
-    const matches = data.user_text.match(user); 
+    const matches = data.user_text.match(user);
     if (matches && matches.length > 1) {
       user = await this.getUserNameFromId(matches[1]);
     } else if (!user) {
-      user = await this.getUserName(data.user);      
+      user = await this.getUserName(data.user);
     }
-    
+
     const poopsRow = await this.PoopModel.findOne({
       where: {
         name: user,
       },
     });
 
-
-    
     let msg = ""
     if (!poopsRow) {
       msg = `${user} has not been pooped... _yet_! :smirk: :poop: `;
@@ -98,33 +116,40 @@ module.exports = class Poop extends BaseStorageModule {
   }
 
   async postLeaderBoard(data) {
-      const poops = await this.PoopModel.findAll({
-        order: [['poops', 'DESC']],
-        limit: 10,
-      });
+    const poops = await this.PoopModel.findAll({
+      order: [['poops', 'DESC']],
+      limit: 10,
+    });
 
-      const fields = [];
-      poops.forEach((plus, index) => {
-        fields.push({
-          title: `${index + 1}. ${plus.get('name')} (${plus.get('poops')} poops)`,
-          short: false,
-        });
+    const fields = [];
+    poops.forEach((plus, index) => {
+      fields.push({
+        title: `${index + 1}. ${plus.get('name')} (${plus.get('poops')} poops)`,
+        short: false,
       });
+    });
 
-      this.bot.postRawMessage(data.channel, {
-        icon_emoji: ':poop:',
-        username: 'PoopBoard',
-        attachments: [
-          {
-            color: '#795548',
-            fields: fields,
-          },
-        ],
-      });
+    this.bot.postRawMessage(data.channel, {
+      icon_emoji: ':poop:',
+      username: 'PoopBoard',
+      attachments: [
+        {
+          color: '#795548',
+          fields: fields,
+        },
+      ],
+    });
   }
 
   aliases() {
     return ['poop-board', 'poopboard', ':poop:', 'poops'];
+  }
+
+  async isBlackListed(channel) {
+    const blacklisted = config.getKey(botParams.blacklist).poop;
+    const currentChannel = await this.bot.getChannelById(channel);
+
+    return blacklisted.includes(currentChannel.name);
   }
 
   help() {
